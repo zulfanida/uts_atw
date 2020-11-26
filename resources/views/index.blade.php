e1d5f3195a08fd0828c4073dff84cb39adae61bd
zulfanida <71075265+zulfanida@users.noreply.github.com>
2020-11-26 22:05:37 +0700

    add form index.blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<style>
+	body
+	{
+		background: #fff;
+		padding: 0;
+		margin: 0;
+	}
+	h2 .zulfa
+	{
+		position: relative;
+		font-size: 3em;
+		font-family: sans-serif;
+		align-items: center;
+		color: #111;
+		margin-left: 20px;
+	}
+</style>
+<body>
+	<h2 class="zulfa">Zulfanida</h2>
+</body>
+</html>
